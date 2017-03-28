53c9a7bf66e489196813e5b865e4b89e34a4ed5d
Alexander Weidinger <alexander.weidinger@tum.de>
2017-03-28 13:15:59 +0200

    Some fixes to TraCI-Proxy, to make it more reliable.

--- TraCI-Proxy.py
+++ TraCI-Proxy.py
@@ -4,6 +4,15 @@ import struct
 import socket
 import argparse
 
+# signal handling
+import functools
+import signal
+import sys
+
+def ask_exit(signame):
+    print("got signal %s: exit" % signame)
+    loop.stop()
+
 # Command line options
 parser = argparse.ArgumentParser(description='TraCI-Proxy - A Proxy to allow multiple TraCI Connections to SUMO')
 parser.add_argument('sumoip', metavar='SUMO_IP', help='IP address of the SUMO TraCI Server')
@@ -19,16 +28,19 @@ s.connect((args.sumoip, args.sumoport))
 
 @asyncio.coroutine
 def handle_client_connect(reader, writer):
-    print('New client connected!')
+    addr = writer.get_extra_info('peername')
+    print('{}:{} connected!'.format(addr[0], addr[1]))
 
     while True:
         # retrieve packet from client
         data_length = yield from reader.read(4)
+        if b'' == data_length:
+            break # fail silently
         length, = struct.unpack('!i', data_length)
-        print('Packet length: {}'.format(length))
+        #print('Packet length: {}'.format(length))
 
         data = yield from reader.read(length - 4)
-        print('Packet data: {}'.format(data_length + data))
+        #print('Packet data: {}'.format(data_length + data))
 
         # forward packet to TraCI-Server
         s.sendall(data_length + data)
@@ -36,24 +48,28 @@ def handle_client_connect(reader, writer):
         # receive answer from TraCI-Server
         data_length = s.recv(4)
         length, = struct.unpack('!i', data_length)
-        print('Packet length: {}'.format(length))
+        #print('Packet length: {}'.format(length))
 
         data = s.recv(length - 4)
-        print('Packet data: {}'.format(data_length + data))
+        #print('Packet data: {}'.format(data_length + data))
 
         # forward answer to client
         writer.write(data_length + data)
     
+
 loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_client_connect, args.proxyip, args.proxyport, loop=loop)
+for signame in ('SIGINT', 'SIGTERM'):
+    loop.add_signal_handler(getattr(signal,
+                                    signame),
+                            functools.partial(ask_exit,
+                                              signame))
+coro = asyncio.start_server(handle_client_connect,
+                            args.proxyip,
+                            args.proxyport,
+                            loop=loop)
 server = loop.run_until_complete(coro)
-
-print('TraCI-Proxy running on {}:{}...'.format(args.proxyip, args.proxyport))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
-
+print('TraCI-Proxy awaiting connections on {}:{}...'.format(args.proxyip, args.proxyport))
+loop.run_forever()
 server.close()
 loop.run_until_complete(server.wait_closed())
 loop.close()
